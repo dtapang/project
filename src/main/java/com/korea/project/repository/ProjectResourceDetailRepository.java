9cfb399d4975ba6176404c75f7a0e96ee9aeea15
Daniel Kanba Tapang <dktapang@gmail.com>
2020-07-28 20:50:29 -0400

    Added controller operations for project resource details

--- src/main/java/com/korea/project/repository/ProjectResourceDetailRepository.java
+++ src/main/java/com/korea/project/repository/ProjectResourceDetailRepository.java
@@ -1,9 +1,19 @@
 package com.korea.project.repository;
 
+import com.korea.project.entity.Project;
 import com.korea.project.entity.ProjectResourceDetail;
+import com.korea.project.entity.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProjectResourceDetailRepository extends  JpaRepository<ProjectResourceDetail, Integer>{
+    List<ProjectResourceDetail> getProjectResourceDetailsByProjectid(Integer id);
+    List<Resource> getResourcesByProjectid(Integer id);
+    Project getByProjectid(Integer id);
+    ProjectResourceDetailRepository getProjectResourceDetailById(Integer id);
+    void deleteProjectResourceDetailById(Integer id);
+    void deleteProjectResourceDetailByProjectid(Integer id);
 }
